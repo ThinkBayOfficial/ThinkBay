3a5d7bc3445b3a31c3a3f4b01f474a1fc41e690e
rahulsabu27 <rahulsabu27@gmail.com>
2024-12-10 08:31:28 +0530

    added messages

--- Helpers/TICons.cs
+++ Helpers/TICons.cs
@@ -8,8 +8,6 @@ namespace ThinkBay.Helpers
 {
     public  class TICons
     {
-        //only for icon
-        //<Label Grid.Row="1" BackgroundColor="Red" VerticalOptions="Center" FontSize="100" Text="{Binding Message,Source={Static local:App.T_Message}}" FontFamily="FontAwesome" TextColor="White"/>
         public string Shopping  { get; set; } = "\uf07a";
     }
 }
--- Helpers/TMessages.cs
+++ Helpers/TMessages.cs
@@ -8,7 +8,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace ThinkBay.Helpers
 {
-    public  class TMessages
+    public  class TMessages 
     {  
         public string LoginHeader { get; set; } = "Let’s sign you in ";
         public string LoginSub { get; set; } = "Welcome Back! We\r\nmissed you.";
@@ -16,8 +16,12 @@ namespace ThinkBay.Helpers
         public string DontHave { get; set; } = " Don’t have an account?";
         public string Create  { get; set; } = " Don’t have an account?";
         public string Create_One { get; set; } = "Create One\r\n";
+        public string Conditions { get; set; } = "Agree on our Terms & Conditions";
+        public string AlreadyHave { get; set; } = " Already have an account?";
 
-
+        public string Login { get; set; } = "Login";
+        public string OTPDescription { get; set; } = "Please enter the 4-digit code sent \r\nto";
+        public string Skip { get; set; } = " Skip";
 
     }
 }
