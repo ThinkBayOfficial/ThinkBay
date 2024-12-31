98879d4959435abdba0be9eef2e7981543de1270
rahulsabu27 <rahulsabu27@gmail.com>
2024-12-31 11:19:15 +0530

    Set Preferences

---
+++ Helpers/T_Preferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkBay.Helpers
+{
+    public class T_Preferences
+    {
+        private static readonly string KEY_LOGIN_DATA;
+
+        public static string LOGIN_DATA
+        {
+            get
+            {
+                return Preferences.Get(KEY_LOGIN_DATA, string.Empty);
+            }
+            set
+            {
+                Preferences.Set(KEY_LOGIN_DATA, value);
+            }
+        }
+    }
+}
